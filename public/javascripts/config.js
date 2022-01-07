01cb4b9a4bfff977989bb549067eff7e781dded4
tibisabau <96194994+tibisabau@users.noreply.github.com>
2022-01-07 23:28:51 +0100

    config client server

---
+++ public/javascripts/config.js
@@ -0,0 +1,5 @@
+(function (exports) {
+    exports.ROWS = 7;
+    exports.COLUMNS = 6;
+    exports.WEB_SOCKET_URL = "ws://localhost:3000"; /* WebSocket URL */
+})(typeof exports === "undefined" ? (this.Setup = {}) : exports);
