496985f066122feb6a9dcc884f3829c230c8ac62
tibisabau <96194994+tibisabau@users.noreply.github.com>
2022-01-08 21:55:25 +0100

    circles css

--- public/javascripts/interactions.js
+++ public/javascripts/interactions.js
@@ -1,8 +1,8 @@
 //ts-check
-function GameState(sb, socket) {
+function GameState(sb) {
     this.playerNumber = null;
     this.MAX_CONNECTED= Setup.MAX_CONNECTED;
-    this.socket = socket;
+    //this.socket = socket;
     this.gameGrid = null;
     this.statusBar = sb;
     this.stack = null;
@@ -25,12 +25,12 @@ GameState.prototype.initializeStack = function () {
 }
 
 GameState.prototype.updateGame = function(clickedSquare) {
-    if(this.stack[parseInt(clickedSquare[4], 10)] > 0) {
+    if(this.stack[parseInt(clickedSquare[4])] > 0) {
             if(this.getPlayerNumber() == 1) {
-                this.gameGrid[this.stack[parseInt(clickedSquare[4], 10)]][parseInt(clickedSquare[5], 10)] = 1;
+                this.gameGrid[this.stack[parseInt(clickedSquare[4])]][parseInt(clickedSquare[5])] = 1;
             }
             else
-                this.gameGrid[this.stack[parseInt(clickedSquare[4], 10)]][parseInt(clickedSquare[5], 10)] = 2;
+                this.gameGrid[this.stack[parseInt(clickedSquare[4])]][parseInt(clickedSquare[5])] = 2;
 
     }
 
@@ -42,6 +42,7 @@ function Grid(gs) {
         Array.from(squares).forEach(function(el) {
             el.addEventListener("click", function singleClick(e) {
                 const clickedSquare = e.target["id"];
+                console.log(parseInt(clickedSquare[4]), parseInt(clickedSquare[5]))
                 gs.updateGame(clickedSquare);
             })
         })
@@ -55,9 +56,11 @@ function StatusBar() {
 }
 
 (function setup() {
-    const socket = new WebSocket(Setup.WEB_SOCKET_URL);
+    //const socket = new WebSocket(Setup.WEB_SOCKET_URL);
     const sb = new StatusBar();
-    const gs = new GameState(sb, socket);
+    const gs = new GameState(sb);
     const grid = new Grid(gs);
-
+    grid.initialize();
+    gs.initializeStack();
+    gs.initializeGrid();
 })();
