35375446477a790a829dbd446b4954ce07f184e2
tibisabau <96194994+tibisabau@users.noreply.github.com>
2022-01-08 18:03:32 +0100

    grid template

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,5 +1,6 @@
 (function (exports) {
-    exports.ROWS = 7;
-    exports.COLUMNS = 6;
+    exports.ROWS = 6;
+    exports.COLUMNS = 7
+    exports.MAX_CONNECTED = 4;
     exports.WEB_SOCKET_URL = "ws://localhost:3000"; /* WebSocket URL */
 })(typeof exports === "undefined" ? (this.Setup = {}) : exports);
--- public/javascripts/interactions.js
+++ public/javascripts/interactions.js
@@ -1,1 +1,26 @@
 //ts-check
+function GameState(gameGrid, sb, socket) {
+    this.playerType = null;
+    this.MAX_CONNECTED= Setup.MAX_CONNECTED;
+    this.socket = socket;
+    this.gameGrid = gameGrid;
+}
+
+GameState.prototype.getPlayerType = function () {
+    return this.playerType;
+};
+
+GameState.prototype.setPlayerType = function (p) {
+    this.playerType = p;
+};
+
+function Grid(gs) {
+    this.initialize = function () {
+        const squares = document.querySelectorAll('grid div');
+        Array.from(squares).forEach(function(el) {
+            el.addEventListener("click", function singleClick(e) {
+                const clickedSquare = e.target["id"]
+            })
+        })
+    }
+}
